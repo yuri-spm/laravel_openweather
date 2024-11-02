44854cc215e14f56cb5d848cdce7227cc3232e7a
Yuri do Monte <yspm.developer@gmail.com>
2024-11-01 23:38:13 -0300

    .\app\Providers\AppServiceProvider.php

--- config/openweather.php
+++ config/openweather.php
@@ -1,5 +1,6 @@
 <?php
-
+//OpenWeather
 return [
+    'api_key' => env('API_OPENWEATHER'),
     'url' => env('OPENWEATHER_BASE_API', 'https://api.openweathermap.org/')
 ];
--- config/services.php
+++ config/services.php
@@ -34,5 +34,9 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    //OpenWeather
+    'openweather' => [
+        'api_key' => env('API_OPENWEATHER'),
+    ],
 
 ];
