b141255788c0e7b738204a71324d8be23c76e00a
Yuri do Monte <yspm.developer@gmail.com>
2024-11-03 21:25:34 -0300

    Fix #5:  OpenWeatherServide finished

--- app/Services/OpenWeatherService.php
+++ app/Services/OpenWeatherService.php
@@ -9,12 +9,24 @@ use function Ramsey\Uuid\v1;
 class OpenWeatherService {
 
     private $api_key;
-
+    
+    /**
+     * __construct
+     *
+     * @return void
+     */
     public function __construct()
     {
         $this->api_key = config('services.openweather.api_key');
     }
-    
+        
+    /**
+     * currentWeather
+     * request day forecast
+     * @param  mixed $city
+     * @param  mixed $uf
+     * @return void
+     */
     public function currentWeather($city, $uf)
     {
         return $this->getAPI('data/2.5/weather', [
@@ -22,15 +34,48 @@ class OpenWeatherService {
             ]
         );
     }
-
+    
+    /**
+     * weatherForecast
+     * request seven days forecast 
+     * @param  mixed $city
+     * @param  mixed $uf
+     * @return void
+     */
     public function weatherForecast($city, $uf)
     {
-        return $this->getAPI('data/2.5/forecast', [
+        $response = $this->getAPI('data/2.5/forecast', [
                 'q' => "{$city},BR-{$uf},BRA"
             ]
         );
-    }
 
+        if (isset($response['list']) && is_array($response['list'])) {
+            $weather_forecast = collect($response['list'])
+                ->take(7)
+                ->map(function($item){
+                    return[
+                       'temp'        => (int)round($item['main']['temp']),
+                       'temp_min'    => (int)round($item['main']['temp_min']),
+                       'temp_max'    => (int)round($item['main']['temp_max']),
+                       'description' => $item['weather'][0]['description'] ?? '',
+                       'icon'        => $item['weather'][0]['icon'] ?? '',
+                    ];
+                });
+
+            return $weather_forecast;
+                
+        }
+
+       
+    }
+    
+    /**
+     * getAPI
+     * request to API
+     * @param  mixed $resource
+     * @param  mixed $params
+     * @return void
+     */
     private function getAPI($resource, $params = [])
     {
         $params['units'] = 'metric';
