6381e9614086be23b03f3fb8c652a8b249577029
Yuri do Monte <yspm.developer@gmail.com>
2024-11-12 19:15:44 -0300

    #4: add title

--- app/Livewire/Openweather.php
+++ app/Livewire/Openweather.php
@@ -5,12 +5,15 @@ namespace App\Livewire;
 use App\Models\State;
 use Livewire\Component;
 use App\Services\OpenWeatherService;
+use Livewire\Attributes\Title;
 
 class Openweather extends Component
 {
     public $result;
     public $city = '';
 
+    
+    #[Title('Open Weather API')]
     public function render()
     {
         return view('livewire.openweather');
