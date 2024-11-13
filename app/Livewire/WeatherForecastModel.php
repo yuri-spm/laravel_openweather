4aed33678856b002d1df89e3cd07a4cfb1ebe4a9
Yuri do Monte <yspm.developer@gmail.com>
2024-11-13 17:24:58 -0300

    #4: add new controller

---
+++ app/Livewire/WeatherForecastModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class WeatherForecastModel extends Component
+{
+    public function render()
+    {
+        return view('livewire.weather-forecast-model');
+    }
+}
