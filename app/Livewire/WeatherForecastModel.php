3fedb39e89349b9c92a2651d2c2aa17838d702e5
Yuri do Monte <yspm.developer@gmail.com>
2024-11-14 10:23:28 -0300

    #10 created view and controller

--- app/Livewire/WeatherForecastModel.php
+++ app/Livewire/WeatherForecastModel.php
@@ -3,11 +3,58 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Services\OpenWeatherService;
 
 class WeatherForecastModel extends Component
 {
+    public $result;
+    public $city;
+    protected $listeners = ['cidadeAtualizada' => 'getCity'];
+    
+    /**
+     * render
+     *
+     * @return void
+     */
     public function render()
     {
         return view('livewire.weather-forecast-model');
+    }
+    
+    /**
+     * showModal
+     *
+     * @return void
+     */
+    public function showModal()
+    {
+        $this->dispatch('closeModal');
+
+        $this->dispatch('openModal');
+    }
+    
+    /**
+     * getCity
+     *
+     * @param  mixed $city
+     * @return void
+     */
+    public function getCity($city)
+    {
+        $this->city = $city;
+        $this->getForecastWeather();
+    }
+    
+    /**
+     * getForecastWeather
+     *
+     * @return void
+     */
+    public function getForecastWeather()
+    {
+        if ($this->city) {
+            $instance = new OpenWeatherService();
+            $this->result = $instance->weatherForecast($this->city);
+        }
     }
 }
--- resources/views/livewire/weather-forecast-model.blade.php
+++ resources/views/livewire/weather-forecast-model.blade.php
@@ -1,9 +1,7 @@
-<div>
-    <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-          <div class="modal-content">
-            ...
-          </div>
-        </div>
+<div class="modal fade bd-example-modal-lg" id="forecastModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+          A cidade que você digitou é: {{ $city }}
       </div>
+  </div>
 </div>
